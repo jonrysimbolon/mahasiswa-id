5955413953d7e083bf5489d48e1685e4ce24542e
Indra Azimi <indraazimi@yahoo.com>
2020-10-01 09:37:05 +0700

    Initial commit

---
+++ app/src/main/java/com/indraazimi/mahasiswaid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.indraazimi.mahasiswaid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
