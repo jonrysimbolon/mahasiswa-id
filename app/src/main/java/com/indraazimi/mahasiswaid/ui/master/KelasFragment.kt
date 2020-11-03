c19430d3712ef7a6c635c3094733df3be958211c
Indra Azimi <indraazimi@yahoo.com>
2020-11-03 17:46:28 +0700

    Membuat two-pane layout untuk tablet

--- app/src/main/java/com/indraazimi/mahasiswaid/ui/master/KelasFragment.kt
+++ app/src/main/java/com/indraazimi/mahasiswaid/ui/master/KelasFragment.kt
@@ -14,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.indraazimi.mahasiswaid.R
@@ -21,16 +22,26 @@ import kotlinx.android.synthetic.main.fragment_kelas.*
 
 class KelasFragment : Fragment() {
 
+    private var isTablet: Boolean = false
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_kelas, null, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        isTablet = resources.getBoolean(R.bool.isTablet)
+
         val kelas = resources.getStringArray(R.array.kelas)
         listView.adapter = ArrayAdapter(requireContext(),
             android.R.layout.simple_list_item_1, android.R.id.text1, kelas)
         listView.setOnItemClickListener { _, _, position, _ ->
+            if (isTablet) {
+                val bundle = bundleOf("kelas" to kelas[position])
+                findNavController().navigate(R.id.action_global_mainFragment, bundle)
+                return@setOnItemClickListener
+            }
+
             findNavController().navigate(
                 KelasFragmentDirections.actionKelasFragmentToMainFragment(kelas[position])
             )
