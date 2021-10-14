42e43e07f6b20f8b29c344b319d24a967728b287
Indra Azimi <indraazimi@yahoo.com>
2021-10-14 10:26:28 +0700

    Menambahkan view binding

--- app/src/main/java/com/indraazimi/mahasiswaid/MainActivity.kt
+++ app/src/main/java/com/indraazimi/mahasiswaid/MainActivity.kt
@@ -11,10 +11,15 @@ package com.indraazimi.mahasiswaid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.indraazimi.mahasiswaid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
